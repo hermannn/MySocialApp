dbfcaad675b6a30cef4f8c73f2fff3bff1ec858f
Dorio Hermann <hermann.dorio@epitech.eu>
2016-12-02 17:18:57 +0100

    Auth with facebook done

--- MySocialApp/SignInViewController.swift
+++ MySocialApp/SignInViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInViewController: UIViewController {
 
@@ -21,5 +24,30 @@ class SignInViewController: UIViewController {
     }
 
 
+    @IBAction func facebookBtnPressed(_ sender: UIButton) {
+        let fbLogin = FBSDKLoginManager()
+        
+        fbLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to Authenticate with facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("User cancelled authentification")
+            }else{
+                print("Succesfully authenticated with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential: credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to Authenticate with firebase - \(error)")
+            }else {
+                print("Succesfully authenticate with firebase")
+            }
+        })
+    }
 }
 
--- Podfile
+++ Podfile
@@ -10,6 +10,9 @@ target 'MySocialApp' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'FacebookCore'
+  pod 'FacebookLogin'
+  pod 'FacebookShare'
 
 
   target 'MySocialAppTests' do
