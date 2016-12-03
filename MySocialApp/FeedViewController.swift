11b7baf7f0c86af8bbe09783833388ef30d9261c
Dorio Hermann <hermann.dorio@epitech.eu>
2016-12-04 00:42:16 +0100

    Post content done and update in tableview

--- MySocialApp/FeedViewController.swift
+++ MySocialApp/FeedViewController.swift
@@ -12,11 +12,13 @@ import SwiftKeychainWrapper
 
 class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var captionTxtField: FancyTextField!
     @IBOutlet weak var AddImage: CircleImageView!
     @IBOutlet weak var mytableview: UITableView!
     var  posts = [Post]()
     var imagepicker:UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,6 +72,7 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage{
+            self.imageSelected = true
            self.AddImage.image = image
         }
         else{
@@ -80,10 +83,56 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
     
     
     //IBAction
+    
     @IBAction func ImagePressed(_ sender: AnyObject) {
         self.present(self.imagepicker, animated: true, completion: nil)
     }
 
+    @IBAction func PostBtnPressed(_ sender: AnyObject) {
+        guard let caption = self.captionTxtField.text, caption != "" else{
+            print("Caption must contain text")
+            return
+        }
+        guard let image = self.AddImage.image, self.imageSelected == true else{
+            print("An image must be selected")
+            return
+        }
+        if let imgData = UIImageJPEGRepresentation(image, 0.2){
+            let imgUid = NSUUID().uuidString // give an unique id for img
+            
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg" // indicate to the firebase that we will store a jpeg
+            
+         DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData, metadata: metadata, completion: { (metadata, error) in
+            if error != nil {
+                print("Unable to upload image to Firebase storage")
+            }else {
+                print("Successfully uploaded image to Firebase")
+                let dlurl = metadata?.downloadURL()?.absoluteString
+                if let url = dlurl {
+                    self.postToFirebase(imageUrl: url)
+                }
+            }
+         })
+        }
+    }
+    
+    func postToFirebase (imageUrl: String){
+        let post: Dictionary<String, AnyObject> = [
+            "caption" : captionTxtField.text! as AnyObject,
+            "imageUrl" : imageUrl as AnyObject,
+            "likes" : 0 as AnyObject
+        ]
+        
+        let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+        firebasePost.setValue(post)
+        
+        self.captionTxtField.text = ""
+        self.imageSelected = false
+        self.AddImage.image = UIImage(named: "add-image")
+        self.mytableview.reloadData()
+    }
+    
     @IBAction func SignOutTapped(_ sender: AnyObject) {
         let keychainresult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         if keychainresult == true {
