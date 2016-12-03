255036077394509a68374edc9b3d4abb8000a5d1
Dorio Hermann <hermann.dorio@epitech.eu>
2016-12-03 12:35:34 +0100

    add custom ui for feed VC

---
+++ MySocialApp/FancyImageView.swift
@@ -0,0 +1,23 @@
+//
+//  FancyImageView.swift
+//  MySocialApp
+//
+//  Created by Hermann Dorio on 03/12/2016.
+//  Copyright © 2016 Hermann Dorio. All rights reserved.
+//
+
+import UIKit
+
+class FancyImageView: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+
+    }
+
+}
